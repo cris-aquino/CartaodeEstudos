40a915a3f02ca1c3093f91fc3ee94e9fc25c3860
cris-aquino <168568536+cris-aquino@users.noreply.github.com>
2024-11-22 10:03:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia', 
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Matemática', 
+    'As grandezas tempo e distância são diretamente proporcionais ou inversamente proporcionais?',
+    'Tempo e distância são inversamente proporcionais'
+)
+
+criaCartao(
+    'Biologia', 
+    'Quais são as bases nitrogenadas do DNA?',
+    'Adenina, Timina, Citosina, Guanina'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o resultado de 20³?',
+    'O resultado é 20.20.20= 8000'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os estados brasileiros da Região Sul?',
+    'Rio Grande do Sul, Santa Catarina e Paraná'
+)
+
+criaCartao(
+    'Geografia',
+    'Quail é a capital do Amazonas?',
+    'A capital do Amazonas é Manaus'
+)
